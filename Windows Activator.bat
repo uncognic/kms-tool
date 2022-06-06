3f3860b8e415a8733bc8f066284e68cfd60441e0
r0sayo <92607100+r0sayo@users.noreply.github.com>
2022-06-06 17:45:05 +0000

    Add files via upload

---
+++ Windows Activator.bat
@@ -0,0 +1,11 @@
+@echo off
+title Windows 10 Pro Activator
+pause
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+timeout 2
+slmgr /skms kms.digiboy.ir
+timeout 2
+slmgr /ato
+print Windows is activated.
+pause
+exit
