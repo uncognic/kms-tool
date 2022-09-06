b79c8002aec3d44f9cea049ae1372cb6300ce4d1
kryxzz <92607100+kryxzz@users.noreply.github.com>
2022-09-06 16:19:21 -0400

    Update Windows.Activator.bat

--- Windows.Activator.bat
+++ Windows.Activator.bat
@@ -1,9 +1,29 @@
 @echo off
-title Windows 10 Pro Activator
-pause
-slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-slmgr /skms kms.digiboy.ir
-slmgr /ato
+title KMS tool for Windows
+echo What would you like to do today?
+echo [1] Activate Windows
+echo [2] Deactivate Windows
+echo [3] Goto GitHub repository
+echo [4] Exit
+CHOICE /C:1234 /N /M "Type a number (1, 2, 3 or 4): "
+IF %ERRORLEVEL% == 1 GOTO activate
+IF %ERRORLEVEL% == 2 GOTO deactivate
+IF %ERRORLEVEL% == 3 start "" https://www.github.com/kryxzz/kms-tool/
+IF %ERRORLEVEL% == 4 exit
+goto start
+:activate
+slmgr //b /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr //b /skms kms.digiboy.ir
+slmgr //b /ato
 echo Windows is activated.
 pause
+goto end
+:deactivate
+slmgr //b /skms kryxzz.github.io
+slmgr //b /upk
+slmgr //b /cpky
+echo Windows has been deactivated.
+pause
+goto end
+:end
 exit
